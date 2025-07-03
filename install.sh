958229e2e6d7fc099d8adbdb82cd8d67d2702fe8
galaxy-cli <kvnallbright@gmail.com>
2025-07-03 05:40:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,8 @@
 # https://github.com/galaxey-cli/mpv
 # mpv3.sh - Text-to-speech and playback utility using Festival, LAME, and MPV
 
-# Updates repos and installs relevant packages for tts.sh
-
 sudo apt update
 sudo apt install -y mpv festival xsel git
-
-# Check if tgpt directory exists before cloning and installing
 
 if [ -d tgpt ]; then
     echo "tgpt is already installed (directory exists). Skipping clone and install."
